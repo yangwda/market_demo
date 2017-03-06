cf9b92d30e7c064569281d9d95c527990270b6c3
Weidong.Yang <hclzywd@163.com>
2017-03-06 09:42:03 +0800

    测试代码

---
+++ market/testsrc/test/cn/yj/market/comm/StringUtil_StringLengthTest.java
@@ -0,0 +1,13 @@
+package test.cn.yj.market.comm;
+
+import org.apache.commons.lang.StringUtils;
+
+public class StringUtil_StringLengthTest {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.length(null));
+		System.out.println(StringUtils.length("1"));
+		System.out.println(StringUtils.length("测试"));
+	}
+
+}
